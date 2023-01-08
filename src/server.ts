d10d3589eb527e9b4ae3623ab225592873e8188f
Robel-Ak <Robelamde21@gmail.com>
2023-01-09 00:12:15 +0300

    add filter

--- src/server.ts
+++ src/server.ts
@@ -30,6 +30,20 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
+  app.get( "/filteredimage", async ( req: Request, res: Response ) => {
+    const image_url: string = (req.query.image_url).toString();
+    if (!image_url) {
+      return res.status(400).send(`Image url is required`);
+    }
+    const regex = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if (!regex.test(image_url)) {
+      return res.status(422).send(`Invalid image url`);
+    }
+    const filteredpath: string = await filterImageFromURL(image_url);
+    res.status(200).sendFile(filteredpath, () => {
+      deleteLocalFiles([filteredpath]);
+    } );
+  } );
   
   // Root Endpoint
   // Displays a simple message to the user
